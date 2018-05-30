30f0215877040c3081bdce7a5536e5321516d883
DESKTOP-AAE5SI9\eoter <e.otero81@gmail.com>
2018-05-29 23:55:15 -0400

    update routes

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactsocial")
+.then(() => console.log("mongoDB connected"))
+.catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello'));
+
+//Use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
